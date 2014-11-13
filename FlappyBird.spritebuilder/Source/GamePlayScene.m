0727ba0d7ea1518c58d88b68275afffd9c53651d
swap2star <swap2star@gmail.com>
2014-11-13 11:00:09 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -6,7 +6,7 @@
 
 - (void)initialize
 {
-    [self addObstacle]
+    [sefl addObstacle];
     // your code here
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
